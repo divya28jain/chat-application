26da1fc03dd32fba9465b0c66ff9da64b9dbf4a1
divya28jain <jain28divya@gmail.com>
2024-06-22 16:21:43 +0530

    third commit finally all done (errors to solve)

---
+++ app/src/main/java/com/example/chatapplication/Login.kt
@@ -0,0 +1,59 @@
+package com.example.chatapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import com.example.chatapplication.ui.theme.ChatApplicationTheme
+import com.google.firebase.auth.FirebaseAuth
+
+class Login : ComponentActivity() {
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login) // Set the XML layout
+
+
+
+        mAuth = FirebaseAuth.getInstance()
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email, password)
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        // Implement login functionality here
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val intent = Intent(this@Login, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                   Toast.makeText(this@Login, "User does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/chatapplication/UserAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.chatapplication
+  import android.content.Context
+  import android.content.Intent
+  import android.view.LayoutInflater
+  import android.view.TextureView
+  import android.view.ViewGroup
+  import androidx.appcompat.view.menu.MenuView.ItemView
+  import androidx.recyclerview.widget.RecyclerView
+  import com.google.firebase.auth.FirebaseAuth
+  import com.google.firebase.ktx.Firebase
+  import org.w3c.dom.Text
+
+class UserAdapter(val context: Context, val userList: ArrayList<User>):
+    RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+     val view: View = LayoutInflater.from(context).inflate(R.layout.user_layout, parent, false)
+     return UserViewHolder(view)
+    }
+
+    override fun getItemCount(): ArrayList<User> {
+        return userList.size
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+      val currentUser = userList[position]
+
+        holder.textName.text = currentUser.name
+
+        holder.itemView.setOnClickListener{
+            val intent = Intent(context,ChatActivity::class.java)
+
+            intent.putExtra("namee", currentUser.name)
+            intent.putExtra("uid",currentUser.vid)
+
+            context.startActivity(intent)
+
+        }
+
+
+    }
+
+    class UserViewHolder(itemView: ItemView) : RecyclerView.ViewHolder(itemView){
+    val textName = itemView.findViewById<TextView>(R.id.txt_name)
+    }
+}
